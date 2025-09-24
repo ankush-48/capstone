d880fa3930b4b0c40d2666102b5372f54956e5c4
Wix Remote Machine <support@wix.com>
2025-09-24 14:53:47 +0000

    initial-commit

---
+++ src/components/Router.tsx
@@ -0,0 +1,40 @@
+import { MemberProvider } from '@/integrations';
+import { createBrowserRouter, RouterProvider, Navigate, Outlet } from 'react-router-dom';
+import { ScrollToTop } from '@/lib/scroll-to-top';
+
+// Layout component that includes ScrollToTop
+function Layout() {
+  return (
+    <>
+      <ScrollToTop />
+      <Outlet />
+    </>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <div>Wix Vibe</div>,
+      },
+      {
+        path: "*",
+        element: <Navigate to="/" replace />,
+      },
+    ],
+  },
+], {
+  basename: import.meta.env.BASE_NAME,
+});
+
+export default function AppRouter() {
+  return (
+    <MemberProvider>
+      <RouterProvider router={router} />
+    </MemberProvider>
+  );
+}
